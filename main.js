ae6b5d50f4838f7ad46bf6dd0dc8641a4284eaf5
hashrock <hashrock@users.noreply.github.com>
2020-08-31 00:49:05 +0900

    store outline

--- main.js
+++ main.js
@@ -34,15 +34,16 @@ function drawSpline() {
         ctx.closePath();
     }
     const spline = new BSpline(pts, degree, true);
-    ctx.beginPath();
     let oldx, oldy, oldp, x, y, p, oldd;
     oldx = spline.calcAt(0)[0];
     oldy = spline.calcAt(0)[1];
     oldp = spline.calcAt(0)[2];
     oldd = 0;
 
+    const leftPoints = []
+    const rightPoints = []
+
     for (let t = 0; t <= 1; t += 0.001) {
-        ctx.moveTo(oldx, oldy);
         const interpol = spline.calcAt(t);
         x = interpol[0];
         y = interpol[1];
@@ -50,17 +51,20 @@ function drawSpline() {
         // ctx.lineTo(x, y);
 
         // 点の間引き…実装してみたが、なにか線の描き味が悪化する気がする
-        // if(Math.pow( x-oldx, 2 ) + Math.pow( y-oldy, 2 ) < 4){
-        //     continue
-        // }
+        if(Math.pow( x-oldx, 2 ) + Math.pow( y-oldy, 2 ) < 32){
+            continue
+        }
 
         const deg = Math.atan2(y - oldy, x - oldx)
         const normal = 3.14 / 2
-        ctx.lineTo(oldx + Math.cos(oldd + normal) * oldp, oldy + Math.sin(oldd + normal) * oldp)
-        ctx.lineTo(x + Math.cos(deg + normal) * p, y + Math.sin(deg + normal) * p)
-        ctx.lineTo(x + Math.cos(deg - normal) * p, y + Math.sin(deg - normal) * p)
-        ctx.lineTo(oldx + Math.cos(oldd - normal) * oldp, oldy + Math.sin(oldd - normal) * oldp)
-        ctx.closePath();
+
+        rightPoints.push([x + Math.cos(deg + normal) * p, y + Math.sin(deg + normal) * p])
+        leftPoints.push([x + Math.cos(deg - normal) * p, y + Math.sin(deg - normal) * p])
+        // ctx.lineTo(oldx + Math.cos(oldd + normal) * oldp, oldy + Math.sin(oldd + normal) * oldp)
+        // ctx.lineTo(x + Math.cos(deg + normal) * p, y + Math.sin(deg + normal) * p)
+        // ctx.lineTo(x + Math.cos(deg - normal) * p, y + Math.sin(deg - normal) * p)
+        // ctx.lineTo(oldx + Math.cos(oldd - normal) * oldp, oldy + Math.sin(oldd - normal) * oldp)
+        // ctx.closePath();
 
         // ctx.arc(x, y, interpol[2] * 10 , 0, Math.PI * 2, false);
         oldx = x;
@@ -68,8 +72,38 @@ function drawSpline() {
         oldd = deg;
         oldp = p;
     }
+    ctx.beginPath();
+    ctx.moveTo(oldx, oldy);
+
+    // for(let i = leftPoints.length - 1; i >= 0; i--){
+    //     console.log(i)
+
+    //     ctx.fillStyle = "rgba(0,255,0,1)";
+    //     ctx.beginPath();
+    //     ctx.arc(leftPoints[i][0], leftPoints[i][1], 5, 0, Math.PI * 2, false);
+    //     ctx.fill();
+    //     ctx.closePath();
+    // }
+    // for(let i = 0; i < rightPoints.length; i++){
+    //     ctx.fillStyle = "rgba(0,255,0,1)";
+    //     ctx.beginPath();
+    //     ctx.arc(rightPoints[i][0], rightPoints[i][1], 5, 0, Math.PI * 2, false);
+    //     ctx.fill();
+    //     ctx.closePath();
+    // }
+
+    for(let i = leftPoints.length - 1; i >= 0; i--){
+        ctx.lineTo(leftPoints[i][0] , leftPoints[i][1])
+    }
+    for(let i = 0; i < rightPoints.length; i++){
+        ctx.lineTo(rightPoints[i][0] , rightPoints[i][1])
+    }
+
+    //なんか先が割れちゃうけどなぜ？
+    ctx.arc(x, y, p , oldd + Math.PI / 2, oldd - Math.PI / 2, true);
+    ctx.closePath();
     ctx.stroke();
-    // ctx.fill()
+    ctx.fill()
 }
 
 function putpoint(e) {
